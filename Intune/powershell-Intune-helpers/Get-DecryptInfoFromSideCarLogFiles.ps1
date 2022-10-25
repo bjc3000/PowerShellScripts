244afaad5a014c0723fd60fe1146e150d1cac994
Brendan Caldwell <brendanc@centra.com.au>
2022-10-25 15:31:57 +1000

    Changing More Files

---
+++ Intune/powershell-Intune-helpers/Get-DecryptInfoFromSideCarLogFiles.ps1
@@ -0,0 +1,11 @@
+<#
+Author:  Oliver Kieselbach
+Script:  Get-DecryptInfoFromSideCarLogFiles.ps1
+#>
+
+# This script was the initial published version, wich is not working anymore! 
+# Please use this new Version 'GetDecryptionInfoFromLogFile.ps1' published with the IntuneWinAppUtilDecoder:
+
+# https://github.com/okieselbach/Intune/blob/master/IntuneWinAppUtilDecoder/IntuneWinAppUtilDecoder/GetDecryptionInfoFromLogFile.ps1
+
+# Many Thanks!
